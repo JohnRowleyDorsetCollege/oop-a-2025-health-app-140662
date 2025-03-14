e3c1a63375185ac3cd54b81930c7b24a4b45dc5e
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-14 14:07:35 +0000

    Created SeedData

---
+++ HealthApp.Razor/Data/SeedData.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthApp.Razor.Data
+{
+    public static class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roles = new string[] { "Admin", "Doctor", "Patient" };
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+
+                if (await userManager.FindByEmailAsync("admin@localhost") == null)
+                {
+                    string password = "Admin@123";
+                    var user = new IdentityUser { UserName = "admin@localhost", Email = "admin@localhost", EmailConfirmed = true };
+                    await userManager.CreateAsync(user, password);
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
+    }
+}
--- HealthApp.Razor/Program.cs
+++ HealthApp.Razor/Program.cs
@@ -29,36 +29,7 @@ else
     app.UseHsts();
 }
 
-
-using (var scope = app.Services.CreateScope())
-{
-    var roles = new string[] { "Admin", "Doctor", "Patient" };
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
-    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
-    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-    foreach (var role in roles)
-    {
-        if (!await roleManager.RoleExistsAsync(role))
-        {
-            await roleManager.CreateAsync(new IdentityRole(role));
-        }
-    }
-
-    if (await userManager.FindByEmailAsync("admin@localhost") == null)
-    {
-        string password = "Admin@123";
-        var user = new IdentityUser { UserName = "admin@localhost", Email = "admin@localhost" , EmailConfirmed=true};
-        await userManager.CreateAsync(user, password);
-
-        userManager.AddToRoleAsync(user, "Admin").Wait();
-
-
-    }
-        
-
-}
+await SeedData.Initialize(app.Services);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
