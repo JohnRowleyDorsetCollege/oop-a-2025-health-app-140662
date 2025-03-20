b72806a7ce986f0be10a14136ccc8569359cf37e
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-03-20 09:49:58 +0000

    seed data

--- HealthApp.Razor/Data/SeedData.cs
+++ HealthApp.Razor/Data/SeedData.cs
@@ -4,9 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace HealthApp.Razor.Data
 {
+    public static class HealthAppRoles
+    {
+        public const string Admin = "Admin";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
     public static class SeedData
     {
         public static async Task Initialize(IServiceProvider serviceProvider)
@@ -17,28 +24,17 @@ namespace HealthApp.Razor.Data
                 var context = services.GetRequiredService<ApplicationDbContext>();
                 var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
                 var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-                var roles = new string[] { "Admin", "Doctor", "Patient" };
+                var roles = new string[] { HealthAppRoles.Patient, HealthAppRoles.Doctor, HealthAppRoles.Admin };
                 foreach (var role in roles)
                 {
                     if (!await roleManager.RoleExistsAsync(role))
                     {
+                        Console.WriteLine($"Creating role {role}");
                         await roleManager.CreateAsync(new IdentityRole(role));
                     }
                 }
-
-                if (await userManager.FindByEmailAsync("admin@localhost") == null)
-                {
-                    string password = "Admin@123";
-                    var user = new IdentityUser { UserName = "admin@localhost", Email = "admin@localhost", EmailConfirmed = true };
-                    await userManager.CreateAsync(user, password);
-                    await userManager.AddToRoleAsync(user, "Admin");
-                }
-
-
-
-
             }
+        
         }
     }
 }
